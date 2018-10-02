ef72edd3f3be03647c012de1ba7785fa5607248f
StanleyChen <chenyi_math@hotmail.com>
2018-10-02 09:31:39 +0700

    學習Python String的第一個練習題目

---
+++ Practice_String1.py
@@ -0,0 +1,12 @@
+#str1 = 'C:\network\newconfig\'  #無法列印的字串
+str2 = 'C:/network/newconfig/'
+str3 = r'C:\network\newconfig'
+# str4 = r'C:\network\newconfig\'  #無法列印的字串
+# str5 = r'C:\network\newconfig''\'  #無法列印的字串
+str6 = r'C:\network\newconfig''\\'
+#print (str1)
+print (str2)
+print (str3)
+# print (str4)
+# print (str5)
+print (str6)
